56d3a1159013a70d8e0f87723d1a11c87160eade
Сергей Андреев <CharlieJr@mail.ru>
2019-07-31 14:04:39 +0300

    Update class Point. Incapsulation

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,17 +13,11 @@ namespace snake
         {
 
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*') ;
             p1.Draw();
            
 
-            Point p2 = new Point();
-            p2.x = 3;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(3, 5, '#');
             p2.Draw();
             
 
