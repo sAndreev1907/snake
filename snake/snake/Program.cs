d08a9c042985d52c0d800fffb8ae24766a4ae670
Сергей Андреев <CharlieJr@mail.ru>
2019-07-31 15:48:01 +0300

    Added List

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,10 +16,37 @@ namespace snake
             Point p1 = new Point(1, 3, '*') ;
             p1.Draw();
            
-
             Point p2 = new Point(3, 5, '#');
             p2.Draw();
-            
+
+            Point p3 = new Point(5, 7, '>');
+            p3.Draw();
+
+            Point p4 = new Point(7, 9, '<');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
 
             Console.ReadLine();
 
